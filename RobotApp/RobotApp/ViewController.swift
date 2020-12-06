a6c59e425f5e19b54f7bf5185dfa966f9b7370b3
marijnschilling <marijn.schilling@payconiq.com>
2020-12-06 12:57:07 +0100

    Setup project

---
+++ RobotApp/RobotApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RobotApp
+//
+//  Created by Marijn Schilling on 06/12/2020.
+//  Copyright © 2020 Marijn Schilling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
